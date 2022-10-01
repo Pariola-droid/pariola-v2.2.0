dc2599975529ed21d667e98e8842b558d01e9bfc
Pariola <kehindephilip15@gmail.com>
2022-10-01 16:04:00 +0100

    ...feat: Project component included

---
+++ data/project.js
@@ -0,0 +1,56 @@
+export const projectData = [
+  {
+    name: 'maurice.design',
+    desc: 'Creative portfolio website for maurice',
+    lang: 'NEXTJS + TYPESCRIPT + AWS',
+    url: {
+      GH: 'https://maurice.design',
+      WB: 'https://maurice.design',
+    },
+  },
+  {
+    name: 'maurice.design',
+    desc: 'Creative portfolio website for maurice',
+    lang: 'NEXTJS + TYPESCRIPT + AWS',
+    url: {
+      GH: 'https://maurice.design',
+      WB: 'https://maurice.design',
+    },
+  },
+  {
+    name: 'maurice.design',
+    desc: 'Creative portfolio website for maurice',
+    lang: 'NEXTJS + TYPESCRIPT + AWS',
+    url: {
+      GH: 'https://maurice.design',
+      WB: 'https://maurice.design',
+    },
+  },
+  {
+    name: 'maurice.design',
+    desc: 'Creative portfolio website for maurice',
+    lang: 'NEXTJS + TYPESCRIPT + AWS',
+    url: {
+      GH: 'https://maurice.design',
+      WB: 'https://maurice.design',
+    },
+  },
+  {
+    name: 'maurice.design',
+    desc: 'Creative portfolio website for maurice',
+    lang: 'NEXTJS + TYPESCRIPT + AWS',
+    url: {
+      GH: 'https://maurice.design',
+      WB: 'https://maurice.design',
+    },
+  },
+  {
+    name: 'maurice.design',
+    desc: 'Creative portfolio website for maurice',
+    lang: 'NEXTJS + TYPESCRIPT + AWS',
+    url: {
+      GH: 'https://maurice.design',
+      WB: 'https://maurice.design',
+    },
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Header from '../components/home/Header';
 import About from '../components/home/About';
 import Caption from '../components/home/Caption';
 import Experience from '../components/home/Experience';
+import Projects from '../components/home/Projects';
 // ::: Main component
 import Footer from '../components/general/Footer';
 
@@ -23,6 +24,7 @@ export default function Home() {
         <About />
         <Caption />
         <Experience />
+        <Projects />
       </main>
       <Footer />
     </div>
