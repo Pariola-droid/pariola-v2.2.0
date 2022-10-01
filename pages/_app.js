1064e1a4014388e1c32f1867fba947273b5cff6f
Pariola <kehindephilip15@gmail.com>
2022-10-01 10:08:34 +0100

    ...feat: file structure, css reset.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import { useEffect, useState } from 'react';
+
+// component imports
+
+import { useRouter } from 'next/router';
+
+import * as ga from '../lib/ga';
+
+// basic styles import
+import '../styles/index.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,81 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta
+            name="description"
+            content="Pariola - Creative Frontend Developer"
+          />
+          <meta
+            name="google-site-verification"
+            content="OCuRTkXD5V9m2XD949t_Ju8_cwv03YlpMrLIL9dntg8"
+          />
+          <meta
+            name="keyword"
+            content="Omopariola Kehinde Philip, pariola.dev, Pariola, Portfolio, Creative Frontend Developer"
+          />
+
+          <meta name="author" content="Pariola - Creative Frontend Developer" />
+          <meta property="og:url" content="https://pariola.dev" />
+          <meta property="og:url" content="https://pariola.dev" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:title"
+            content="Pariola - Creative Frontend Developer"
+          />
+          <meta
+            property="og:description"
+            content="Pariola - Creative Frontend Developer"
+          />
+          <meta property="og:image" content="/meta-pariola.svg" />
+          <meta
+            property="og:site_name"
+            content="Pariola - Creative Frontend Developer"
+          />
+
+          <meta name="twitter:card" content="summary" />
+          <meta
+            name="twitter:site"
+            content="Pariola - Creative Frontend Developer"
+          />
+          <meta
+            name="twitter:title"
+            content="Pariola - Creative Frontend Developer"
+          />
+          <meta
+            name="twitter:description"
+            content="Pariola - Creative Frontend Developer"
+          />
+          <meta name="twitter:creator" content="@ipariola" />
+          <meta name="twitter:image" content="/meta-pariola.svg" />
+          <link rel="icon" href="/favicon.svg" />
+
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
