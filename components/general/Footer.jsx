894052fee3dc80945ed2d896491f12e7ac34ad0b
Pariola <kehindephilip15@gmail.com>
2022-10-21 15:36:12 +0100

    ...fixes: mail address link

--- components/general/Footer.jsx
+++ components/general/Footer.jsx
@@ -13,7 +13,7 @@ export default function Footer() {
 
             <ul>
               <li>
-                <a href="hi@pariola.dev">hi@pariola.dev</a>
+                <a href="mailto:hi@pariola.dev">hi@pariola.dev</a>
               </li>
               <li>
                 <a
