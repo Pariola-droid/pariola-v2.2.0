06e13c44935e38d926f584dfa9c8db2dce3710f6
Pariola <kehindephilip15@gmail.com>
2023-02-19 17:40:48 +0100

    ...fixes: esLint errors

--- pages/404.tsx
+++ pages/404.tsx
@@ -9,9 +9,9 @@ export default function NotFound() {
         <h1>404 – Unavailable For Legal Reasons</h1>
         <p className="text-gray-600 dark:text-gray-400 mb-8">
           Why show a generic 404 when I can make it sound mysterious? It seems
-          you've found something that used to exist, or you spelled something
-          wrong. I'm guessing you spelled something wrong. Can you double check
-          that URL?
+          you&apos;ve found something that used to exist, or you spelled
+          something wrong. I&apos;m guessing you spelled something wrong. Can
+          you double check that URL?
         </p>
         <Link href="/">Return Home</Link>
       </div>
