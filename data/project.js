bf2bbc8288a89cdae3a8a9c2dc6b0cf0e2710276
Pariola <kehindephilip15@gmail.com>
2023-08-07 00:47:48 +0100

    chore: updated project data

--- data/project.js
+++ data/project.js
@@ -5,11 +5,17 @@ export const projectData = [
     lang: 'parcel + html + gsap + sass',
     link: 'https://maurice-victor.netlify.app',
   },
+  // {
+  //   name: 'ChurchRUSH',
+  //   desc: 'The complete operational stack for your church',
+  //   lang: 'NextJS + GraphQL',
+  //   link: 'https://churchrush.com',
+  // },
   {
-    name: 'ChurchRUSH',
-    desc: 'The complete operational stack for your church',
-    lang: 'NextJS + GraphQL',
-    link: 'https://churchrush.com',
+    name: 'Circle Finance',
+    desc: 'Unlock your Financial Potential',
+    lang: 'NEXTJS + SASS',
+    link: 'https://tryklas.com',
   },
   {
     name: 'Klas',
