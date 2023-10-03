b486777caf9c4545a5c90d08c258ce099246a382
Pariola <kehindephilip15@gmail.com>
2023-10-03 11:02:33 +0100

    chore: updated the experience section

--- components/home/Experience.tsx
+++ components/home/Experience.tsx
@@ -18,15 +18,17 @@ export default function Experience() {
               I&apos;ve built products for companies and individuals around
               africa ranging from marketing websites to complex solutions and
               enterprise apps with focus on fast, elegant and accessible user
-              experiences. Currently, I work at Diool as a frontend engineer in
+              experiences. Currently, I work at{' '}
+              <a href="https://diool.com">Diool</a> as a frontend engineer in
               the growth team.
             </p>
 
             <p>
-              Before now, I worked as the Founding Frontend Engineer at Klas,
-              crafting thoughtful and inclusive experiences that adhere to web
-              standards. Prior to Klas, I was a front engineer contractor with
-              Regnify, building cool products that users love
+              Before now, I worked as the Founding Frontend Engineer at{' '}
+              <a href="https://tryklas.com">Klas</a>, crafting thoughtful and
+              inclusive experiences that adhere to web standards. Prior to Klas,
+              I was a front engineer contractor with Regnify and Haxell,
+              building digital products that users love
             </p>
           </article>
         </div>
