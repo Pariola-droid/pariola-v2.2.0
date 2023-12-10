418a576cab09565108162334fb20b9bcd9dcb59e
Pariola <kehindephilip15@gmail.com>
2023-12-10 11:35:01 +0100

    chore: set of dummy pages for a test IG app i created

---
+++ pages/data-deletion.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from 'react';
+import Layout from '../layout/Layout';
+
+export default function DataDeletion() {
+  return (
+    <Layout>
+      <main>Coming Soon...</main>
+    </Layout>
+  );
+}
---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from 'react';
+import Layout from '../layout/Layout';
+
+export default function PrivacyPolicy() {
+  return (
+    <Layout>
+      <main>Coming Soon...</main>
+    </Layout>
+  );
+}
