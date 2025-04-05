ca8bb0e03eaf1521046714c3b9d2cb7e9004e81b
Pariola <kehindephilip15@gmail.com>
2025-04-05 04:10:48 +0100

    chore: remove commented-out config for experimental-edge runtime in now-playing API

--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -3,9 +3,9 @@ export const runtime = 'experimental-edge';
 import { getNowPlaying } from 'lib/spotify';
 import { type NextRequest } from 'next/server';
 
-export const config = {
-  runtime: 'experimental-edge',
-};
+// export const config = {
+//   runtime: 'experimental-edge',
+// };
 
 export default async function handler(req: NextRequest) {
   const response = await getNowPlaying();
