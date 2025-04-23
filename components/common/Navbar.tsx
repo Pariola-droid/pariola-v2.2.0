73c67466d3aa8842b6b282e780e102a1cc81c3fc
Pariola <kehindephilip15@gmail.com>
2025-04-23 19:45:45 +0100

    chore: refactor Navbar component to use Tooltip and enhance status display

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,6 +1,8 @@
 import styles from './styles/navbar.module.scss';
 
-import NowPlaying from 'components/addons/Spotify';
+import { Tooltip } from 'react-tippy';
+import 'react-tippy/dist/tippy.css';
+// import NowPlaying from 'components/addons/Spotify';
 import Link from 'next/link';
 
 export default function Navbar() {
@@ -11,7 +13,30 @@ export default function Navbar() {
           <Link href="/">pariola.</Link>
         </h1>
 
-        <NowPlaying />
+        <div className={styles.statusSlot}>
+          <Tooltip
+            html={
+              <div>
+                <div className={styles.songArtist}>Zeity</div>
+              </div>
+            }
+            position="top"
+            duration={200}
+            size="small"
+          >
+            <div className={styles.statusIcon}>🚧</div>
+          </Tooltip>
+
+          <a
+            href="https://www.zeity.pariola.work"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className={styles.statusTitle}>What&apos;s new?</div>
+          </a>
+        </div>
+
+        {/* <NowPlaying /> */}
       </div>
     </nav>
   );
